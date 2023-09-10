c5d742283ed969b92c39b1fbf979a71bed2c328e
Kingsley Amadi <akingsley655@gmail.com>
2023-09-10 13:53:27 -0400

    my first Rust

---
+++ src/print.rs
@@ -0,0 +1,7 @@
+pub fn run() {
+    // printing to console
+    println!("Testing my new Rusty print function!");
+
+    //Basic formatting
+    println!(" {} is made from {} ", "Bread" , "flour");
+}
